a55a57577fdcfb74329b435d3034a47e8f4b1f5e
Sebastian <agudelocortess@gmail.com>
2024-03-04 11:40:30 -0500

    Segundo y ultimo commit :)

--- src/components/Json/CardContent.js
+++ src/components/Json/CardContent.js
@@ -2,7 +2,7 @@ export const CardContent =[
     {
         _id: 1,
         title: "IMC Java",
-        image: "../../../public/Java.png",
+        image: "https://th.bing.com/th/id/R.2162c94aad65e20c0813ca010620b84c?rik=h8M6mTXF%2bT6p%2fQ&pid=ImgRaw&r=0",
         description: "Proyecto masa moscular Java",
         enlace:"https://github.com/sebatian-cortes/IMC_Java"
 
@@ -11,7 +11,7 @@ export const CardContent =[
     {
         _id: 2,
         title: "Bot Musica",
-        image: "../../../public/bot.png",
+        image: "https://i.ytimg.com/vi/ZFwdnHFvE9c/maxresdefault.jpg",
         description: "Bot Musica Python",
         enlace:"https://github.com/sebatian-cortes/Bot_Musica"
     
@@ -19,7 +19,7 @@ export const CardContent =[
     {
         _id: 3,
         title: "Bleux",
-        image: "../../../public/bleux.png",
+        image: "https://th.bing.com/th/id/R.d115a3daecb991316b31ccef64146930?rik=7MzVf%2bhxuZVHBQ&pid=ImgRaw&r=0",
         description: "Pagina Bleux",
         enlace:"https://github.com/sebatian-cortes/Bleux.git"
     
